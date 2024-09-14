ec15208a5d7fee71c401dd639f735d40f7214ed9
bucccket <nicolas.lisgaras@gmail.com>
2024-09-14 18:27:00 +0200

    change toggle term sizes to be dependent on type

--- config/toggleterm.nix
+++ config/toggleterm.nix
@@ -7,7 +7,15 @@
       direction = "vertical";
       hide_numbers = true;
       start_in_insert = true;
-      size=40;
+      size = ''
+        function(term)
+            if term.direction == "horizontal" then
+              return 15
+            elseif term.direction == "vertical" then
+              return vim.o.columns * 0.4
+            end
+          end
+      '';
     };
   };
 }
