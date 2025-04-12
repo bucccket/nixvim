79774641e881cfa9f41ebc0a7f7f5dd39ce74ad4
Nicolas Lisgaras <nli@lambda9.de>
2025-04-12 04:00:16 +0200

    fixed options. oops

--- config/default.nix
+++ config/default.nix
@@ -18,6 +18,7 @@
     ./lualine.nix
     ./none-ls.nix
     ./nvim-tree.nix
+    ./options.nix
     ./refactoring.nix
     ./telescope.nix
     ./toggleterm.nix
@@ -29,27 +30,6 @@
     ./which-key.nix
     ./wilder.nix
   ];
-  
-  config.opts = {
-
-    updatetime = 100; # Faster completion
-
-    number = true;
-    relativenumber = true;
-
-    autoindent = true;
-    clipboard = "unnamedplus";
-    expandtab = true;
-    shiftwidth = 2;
-    tabstop = 2;
-
-    ignorecase = true;
-    incsearch = true;
-    smartcase = true;
-    wildmode = "list:longest";
-
-    undofile = true; # Built-in persistent undo
-  };
 
   extraPlugins = with pkgs.vimPlugins; [
     haskell-tools-nvim
---
+++ config/options.nix
@@ -0,0 +1,22 @@
+{
+  config.opts = {
+
+    updatetime = 100; # Faster completion
+
+    number = true;
+    relativenumber = true;
+
+    autoindent = true;
+    clipboard = "unnamedplus";
+    expandtab = true;
+    shiftwidth = 2;
+    tabstop = 2;
+
+    ignorecase = true;
+    incsearch = true;
+    smartcase = true;
+    wildmode = "list:longest";
+
+    undofile = true; # Built-in persistent undo
+  };
+}
