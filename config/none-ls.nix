b667a711db7ad1304d4eb9b43da93632ea86bc61
Nicolas Lisgaras <nli@lambda9.de>
2025-04-21 03:50:31 +0200

    added pydoclint

--- config/none-ls.nix
+++ config/none-ls.nix
@@ -13,6 +13,7 @@
         ktlint.enable = true;
         mypy.enable = true;
         pylint.enable = true;
+        pydoclint.enable = true;
         semgrep.enable = true;
         statix.enable = true;
         stylelint.enable = true;
